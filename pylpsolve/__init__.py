e484c6f1c981f67c9575f9ea0327cdc8ef04e261
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2016-06-14 16:41:59 -0400

    Use relative import in __init__

--- pylpsolve/__init__.py
+++ pylpsolve/__init__.py
@@ -16,4 +16,4 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 from __future__ import absolute_import
-from pylpsolve import LP, LPException
+from .pylpsolve import LP, LPException
